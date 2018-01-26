63b8fa5263cad5c007bb5b8609e2baea24e0e29e
Xwx100 <793486544@qq.com>
2018-01-26 23:54:22 +0800

    一

---
+++ (对象)Object.py
@@ -0,0 +1,13 @@
+# python一切皆对象，数字、字符串、列表、字典、元组等
+# 任何对象都有三个关键属性：标识，值，类型
+i = 10
+i2 = '10'
+print(i)
+print(i2)
+nums = []
+for num in range(100):
+    nums.append(num)
+print(nums)
+dicts = {'name':'zhang','age':23,}
+print(dicts)
+# 等号右边是对象，左边是给对象取的名字。
