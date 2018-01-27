eaada1038610cf6e8bc5f63464607447c6ef8f95
Xwx100 <793486544@qq.com>
2018-01-27 23:51:49 +0800

    一

---
+++ Object-类型.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+i = 8
+f = 8.00
+s = 'string'
+t = (1,2,3)
+l = [1,2,3]
+d = {'a':1,'b':'2'}
+print(type(i))
+print(type(f))
+print(type(s))
+print(type(t))
+print(type(l))
+print(type(d))
